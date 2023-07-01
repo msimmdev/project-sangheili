2940b8c4dafd6c75fecd51e095107969e3416a4b
Michael Simm <michael@simm.dev>
2023-07-01 22:54:42 +0100

    Fix docker mount

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,5 +8,5 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
