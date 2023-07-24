06db924ed4dddfe797bcf788983438bd4db2aa63
Michael Simm <michael@simm.dev>
2023-07-24 21:39:44 +0100

    Include types in docker files

--- types/src/recipe.ts
+++ types/src/recipe.ts
@@ -1,14 +1,18 @@
+import { UUID } from "crypto";
 import RecipeIngredient from "./recipe-ingredient";
 import RecipeSection from "./recipe-section";
 import { Duration } from "moment";
 
 type Recipe = {
+  id: UUID;
   name: string;
   description: string;
   ingredientList: RecipeIngredient[];
   productList: RecipeIngredient[];
-  method: RecipeSection[];
-  totalTime: Date;
+  sections: RecipeSection[];
+  totalTime: Duration | null;
+  prepTime: Duration | null;
+  cookTime: Duration | null;
 };
 
 export default Recipe;
