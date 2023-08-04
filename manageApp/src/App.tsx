1160485cf42e3dbb6b9abd6c29a987af7956b557
Michael Simm <michael@simm.dev>
2023-08-04 21:29:46 +0100

    Add OAuth Integration

--- manageApp/src/App.tsx
+++ manageApp/src/App.tsx
@@ -1,36 +1,56 @@
 import { Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <>
-      <div className="header-bar">
-        <header className="container">
-          <div className="header-title">Project Sangheili</div>
-          <div className="header-search">
-            <form
-              action="/search"
-              method="GET"
-              role="search"
-              autoComplete="off"
-              className="search-form"
-            >
-              <input type="text" placeholder="Search..." name="p" />
-              <button>
-                <span className="material-symbols-outlined">search</span>
-              </button>
-            </form>
-          </div>
-          <div className="header-widgets">Widgets</div>
-        </header>
-      </div>
-      <main className="container">
-        <Outlet />
-      </main>
-      <div className="footer-bar">
-        <footer className="container">This is a footer</footer>
-      </div>
-    </>
-  );
+  const { isLoading, isAuthenticated, error, loginWithRedirect } = useAuth0();
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  useEffect(() => {
+    if (!isLoading && !isAuthenticated) {
+      loginWithRedirect();
+    }
+  }, [isAuthenticated, isLoading]);
+
+  if (isAuthenticated) {
+    return (
+      <>
+        <div className="header-bar">
+          <header className="container">
+            <div className="header-title">Project Sangheili</div>
+            <div className="header-search">
+              <form
+                action="/search"
+                method="GET"
+                role="search"
+                autoComplete="off"
+                className="search-form"
+              >
+                <input type="text" placeholder="Search..." name="p" />
+                <button>
+                  <span className="material-symbols-outlined">search</span>
+                </button>
+              </form>
+            </div>
+            <div className="header-widgets">Widgets</div>
+          </header>
+        </div>
+        <main className="container">
+          <Outlet />
+        </main>
+        <div className="footer-bar">
+          <footer className="container">This is a footer</footer>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- manageApp/src/main.tsx
+++ manageApp/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.tsx";
 import "./index.css";
 import { EditRecipe } from "./pages";
@@ -19,7 +20,15 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Auth0Provider
+    domain="dev-refc10d66hh7fj8n.eu.auth0.com"
+    clientId="A8tg21fOrDruSRBVR3SYT15ZvtvyDLf7"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Auth0Provider>
 );
