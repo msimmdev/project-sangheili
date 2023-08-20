02d2dbde7bbfb874bfffea3f2f4926c5ba0782fe
Michael Simm <michael@simm.dev>
2023-08-20 14:59:47 +0100

    Disable setting default user

--- api/crud/src/middleware/token-user.ts
+++ api/crud/src/middleware/token-user.ts
@@ -10,19 +10,9 @@ function tokenUser(): (
   return async (req, res, next) => {
     if (typeof req.userToken !== "undefined") {
       if (typeof req.userToken.sub !== "undefined") {
-        let user = await getAppUser(req.userToken.sub || "");
+        let user = await getAppUser(req.userToken.sub);
         if (user !== null) {
           req.user = user;
-        } else {
-          const parseResult = await AppUserSchema.safeParseAsync({
-            externalId: req.userToken.sub,
-            name: req.userToken.name,
-            roles: ["PrivateContributer"],
-          });
-
-          if (parseResult.success) {
-            req.user = parseResult.data;
-          }
         }
       }
     }
